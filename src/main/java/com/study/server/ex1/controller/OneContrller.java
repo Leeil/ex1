b39c5f5a9c0b17ecc0ecc52a62e389a7991d4472
Leeli <likyeong961@naver.com>
2020-11-01 13:04:37 +0900

    firstfile

---
+++ src/main/java/com/study/server/ex1/controller/OneContrller.java
@@ -0,0 +1,35 @@
+package com.study.server.ex1.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Random;
+import java.util.Scanner;
+
+@Controller
+public class OneContrller {
+    @GetMapping("/one")
+    public String one (){
+        return "main/one";
+    }
+
+    @GetMapping("/one/user")
+    public ModelAndView view(@RequestParam int user)
+    {
+        int computer =(int)(Math.random()*3)+1;
+        ModelAndView result = new ModelAndView("main/user");//페이지로 넣을 수 있는 곳
+
+        if((computer == 1 && user == 2) || (computer == 2 && user == 3) || (computer == 3 && user == 1)) {
+            result.getModel().put("user","승리");
+        }else if(computer == user) {
+            result.getModel().put("user","무승부");
+        }else {
+            result.getModel().put("user","패배");
+        }
+        return result;
+
+
+    }
+
+}
