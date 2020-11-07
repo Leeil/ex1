7cb82103fe7042d1d2c1f3ba1070fef5ca90d5fa
Leeli <likyeong961@naver.com>
2020-11-08 00:11:13 +0900

    ex1(Incom)

---
+++ src/main/java/com/study/server/ex1/controller/indexController.java
@@ -0,0 +1,13 @@
+package com.study.server.ex1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+
+    @GetMapping({"", "/"})
+    public String getIndex() {
+        return "index";
+    }
+}
